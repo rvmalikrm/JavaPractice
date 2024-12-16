167eb58d3ed90f51d8691f29f8619119fba76387
rvmalikrm <rm.rvmalik@gmail.com>
2024-12-16 14:29:13 +0530

    Commit average programe

---
+++ src/PracticeTest/Lab16Dec_Part2.java
@@ -0,0 +1,11 @@
+package PracticeTest;
+
+public class Lab16Dec_Part2 {
+    public static void main(String[] args) {
+        int a=34;
+        int b=54;
+        int c= 393;
+        int avg =(a+b+c)/3;
+        System.out.println(avg);
+    }
+}
