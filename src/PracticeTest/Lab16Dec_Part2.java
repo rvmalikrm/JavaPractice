895766b1c000959ca28194196173a8ab68c641d1
rvmalikrm <rm.rvmalik@gmail.com>
2024-12-17 11:29:27 +0530

    Commit Let_us_java_D_a

--- src/PracticeTest/Lab16Dec_Part2.java
+++ src/PracticeTest/Lab16Dec_Part2.java
@@ -2,10 +2,20 @@ package PracticeTest;
 
 public class Lab16Dec_Part2 {
     public static void main(String[] args) {
+        float d= 314.562f * 150 ;
+        char e ='3';
+        //3.14f * r * r * h = vol_of_cyl ;
         int a=34;
         int b=54;
         int c= 393;
         int avg =(a+b+c)/3;
         System.out.println(avg);
+        System.out.println(d);
+        System.out.println(e);
+        //System.out.println(3.14f * r * r * h);
+        //----->/* This is a /* comment */ */
+        /* This is a comment
+
+         */
     }
 }
---
+++ src/PracticeTest/Lab17Dec24_Let_us_java_D_a.java
@@ -0,0 +1,12 @@
+package PracticeTest;
+
+public class Lab17Dec24_Let_us_java_D_a {
+    public static void main(String[] args) {
+        int Basic_Salary =30000;
+        int DA =((Basic_Salary*40)/100);
+        int RA =((Basic_Salary*20)/100);
+        int Gross_Salary = Basic_Salary+DA+RA;
+        System.out.println(Gross_Salary + " Thousand");
+
+    }
+}
