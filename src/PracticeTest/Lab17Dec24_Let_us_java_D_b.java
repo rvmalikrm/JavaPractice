1974ebbe2be0965d09e0ff2cfb820368650b01df
rvmalikrm <rm.rvmalik@gmail.com>
2024-12-17 11:46:19 +0530

    Commit Let_us_java_D_b

---
+++ src/PracticeTest/Lab17Dec24_Let_us_java_D_b.java
@@ -0,0 +1,19 @@
+package PracticeTest;
+
+public class Lab17Dec24_Let_us_java_D_b {
+    public static void main(String[] args) {
+       int distance= 1700;
+       int distance_in_meters= distance*1000;
+       double distance_in_feet= distance*3280.84;
+       int distance_in_inches= distance* 39370;
+       long distance_in_centimeters= distance*100000;
+
+        System.out.println(" The distance between Noida_and_Dubai in KM is  " + distance );
+        System.out.println(" The distance between Noida_and_Dubai in meters is  " + distance_in_meters );
+        System.out.println(" The distance between Noida_and_Dubai in feet is  " + distance_in_feet );
+        System.out.println(" The distance between Noida_and_Dubai in inches is  " + distance_in_inches );
+        System.out.println(" The distance between Noida_and_Dubai in centimeters is  " + distance_in_centimeters );
+
+
+    }
+}
