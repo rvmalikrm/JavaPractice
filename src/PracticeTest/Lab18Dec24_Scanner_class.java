df23100f11a447921d872d8fd1ac8434b84f5c69
rvmalikrm <rm.rvmalik@gmail.com>
2024-12-18 12:17:26 +0530

    Commit Let_us_java Data Tyep boolean p2

---
+++ src/PracticeTest/Lab18Dec24_Scanner_class.java
@@ -0,0 +1,19 @@
+package PracticeTest;
+import java.util.Scanner;
+
+public class Lab18Dec24_Scanner_class {
+    public static void main(String[] args) {
+        // Calculation of simple interest
+        float p, r, si;
+        int n;
+        System.out.println("Enter the value of p, n and r :");
+        Scanner sc =new Scanner(System.in);
+        p=sc.nextFloat();
+        n=sc.nextInt();
+        r =sc.nextFloat();
+        si= ((p*n*r)/100);
+        System.out.println("Simple Intrest is "+si);
+
+
+    }
+}
