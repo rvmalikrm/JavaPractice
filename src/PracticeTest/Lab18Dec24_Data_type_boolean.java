6aef1d17e1e6baf6fb140d1ff33d7c37516800ae
rvmalikrm <rm.rvmalik@gmail.com>
2024-12-18 11:56:48 +0530

    Commit Let_us_java Data Tyep boolean

---
+++ src/PracticeTest/Lab18Dec24_Data_type_boolean.java
@@ -0,0 +1,8 @@
+package PracticeTest;
+
+public class Lab18Dec24_Data_type_boolean {
+    public static void main(String[] args) {
+        boolean a = false;
+        System.out.println("a = " +a);
+    }
+}
