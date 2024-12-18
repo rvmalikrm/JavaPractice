72ef09a54119458ae070bcbe104e18254d17e718
rvmalikrm <rm.rvmalik@gmail.com>
2024-12-18 12:00:04 +0530

    Commit Let_us_java Data Tyep boolean p2

--- src/PracticeTest/Lab18Dec24_Data_type_boolean.java
+++ src/PracticeTest/Lab18Dec24_Data_type_boolean.java
@@ -4,5 +4,13 @@ public class Lab18Dec24_Data_type_boolean {
     public static void main(String[] args) {
         boolean a = false;
         System.out.println("a = " +a);
+        boolean b = 4>2;
+        System.out.println("b = " + b);
+
+        int x; boolean y;
+        //x= 3<4;
+        y = 3<4;
+        //System.out.println(x);
+        System.out.println(y);
     }
 }
