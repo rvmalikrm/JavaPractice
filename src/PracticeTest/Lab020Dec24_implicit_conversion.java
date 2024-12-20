e0cc243c1b757618b18fe4890820e07570722048
rvmalikrm <rm.rvmalik@gmail.com>
2024-12-20 15:38:17 +0530

    Commit Let_us_java Implicit conversion

---
+++ src/PracticeTest/Lab020Dec24_implicit_conversion.java
@@ -0,0 +1,12 @@
+package PracticeTest;
+
+public class Lab020Dec24_implicit_conversion {
+    public static void main(String[] args) {
+        byte a=100;
+        byte b=50;
+        //byte c=a+b; resulting in int so now byte allowed here
+        int d=a+b;
+        //System.out.println(c);
+        System.out.println(d);
+    }
+}
