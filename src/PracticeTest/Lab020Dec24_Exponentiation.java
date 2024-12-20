a44032c39f66a87cdf092790ab6870fdbb5d6ce8
rvmalikrm <rm.rvmalik@gmail.com>
2024-12-20 15:21:58 +0530

    Commit Let_us_java Math_Paw

---
+++ src/PracticeTest/Lab020Dec24_Exponentiation.java
@@ -0,0 +1,9 @@
+package PracticeTest;
+import java.lang.Math;
+public class Lab020Dec24_Exponentiation {
+    public static void main(String[] args) {
+        double d;
+        d= Math.pow(3.0,2.0);
+        System.out.println(d);
+    }
+}
--- src/PracticeTest/Lab19Dec24_data_type.java
+++ src/PracticeTest/Lab19Dec24_data_type.java
@@ -6,5 +6,24 @@ public class Lab19Dec24_data_type {
         // int b=3; a=4; This will work
         float a=1.2f, b=a+3.5f;
         System.out.println(b);
+        int c, d, e,f=21;
+        c=d=e=10;
+        System.out.println(c);
+        System.out.println(d);
+        System.out.println(e);
+        System.out.println(f);
+
+        /*However, the following statement would not work:
+        int g = h = i = j = 10 ;
+        This is because we are trying to use b (to assign to a) before defining
+        it.
+        -------------------------
+        In addition to the division operator Java also provides a modular
+division operator (%). This operator returns the remainder on
+dividing one operand with another. Thus the expression 10 / 2 yields
+5, whereas, 10 % 2 yields 0. Note that on using %, the sign of the
+remainder is always same as the sign of the numerator. Thus -5 % 2
+yields -1, whereas, 5 % -2 yields 1
+        */
     }
 }
