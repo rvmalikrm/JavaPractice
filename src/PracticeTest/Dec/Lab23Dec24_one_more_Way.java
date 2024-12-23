3f423dcc5c73e1acac7cc5976afbf8b8b427c464
rvmalikrm <rm.rvmalik@gmail.com>
2024-12-23 12:09:54 +0530

    Commit Let_us_java 23 Dec more way to input value

--- src/PracticeTest/Dec/Lab23Dec24_one_more_Way.java
+++ src/PracticeTest/Dec/Lab23Dec24_one_more_Way.java
@@ -16,6 +16,14 @@ public class Lab23Dec24_one_more_Way {
 
         System.out.println("Enter the valude of p,n and r ");
         p=Float.parseFloat(br.readLine());
+
+        /* The numeric values that we supply to the three readLine( ) functions is
+        treated as string of characters, rather than as numbers. So we need to
+        Chapter 3: Java Data Types and Instructions 41
+        convert the strings into numbers. This conversion is done by the
+        parseFloat( ) and parseInt( ) functions. These functions belong to a
+        library classes called Float and Integer, respectively. */
+
         n=Integer.parseInt(br.readLine());
         r=Float.parseFloat(br.readLine());
         si=(p*n*r)/100;
