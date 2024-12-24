1140e30ff89fd415f8033dfe7e64d3467caf6c62
rvmalikrm <rm.rvmalik@gmail.com>
2024-12-24 13:44:08 +0530

    Commit Let_us_java 24 dec format output

---
+++ src/PracticeTest/Dec/Lab24Dec24_format.java
@@ -0,0 +1,10 @@
+package PracticeTest.Dec;
+
+public class Lab24Dec24_format {
+    public static void main(String[] args) {
+        int avg=65;
+        int per=66;
+        System.out.println("Output");
+        System.out.println("Average = " +avg + "\n" +"Percentagge " + per);
+    }
+}
