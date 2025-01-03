a09c798375de3597b4644b844ad6817e0eabe1ef
rvmalikrm <rm.rvmalik@gmail.com>
2025-01-03 17:06:16 +0530

    Commit for data type Lab3Jan25_A_c

---
+++ src/PracticeTest/Jan/Lab3Jan25_A_c.java
@@ -0,0 +1,13 @@
+package PracticeTest.Jan;
+
+public class Lab3Jan25_A_c {
+    public static void main(String[] args) {
+
+        int qui = 4,god = 2,add = 2;
+        int s;
+        s = qui * add / 4 - 6 / 2 + 2 / 3 * 6 / god ;
+       // (qui = 4, add = 2, god = 2, assume s to be an int)
+
+        System.out.println((int)s);
+    }
+}
