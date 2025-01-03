2a8b975c2c7149bb72ccfa4fe4c71d4075d18ef1
rvmalikrm <rm.rvmalik@gmail.com>
2025-01-03 17:03:54 +0530

    Lab3Jan25_A_b commit for data type

---
+++ src/PracticeTest/Jan/Lab3Jan25_A_b.java
@@ -0,0 +1,12 @@
+package PracticeTest.Jan;
+
+public class Lab3Jan25_A_b {
+    public static void main(String[] args) {
+        float tig = 3.2f;
+        int ink = 4,act = 1;
+        int on;
+        on = ink * act / 2 + 3 / 2 * act + 2 + (int)tig;
+
+        System.out.println((int)on);
+    }
+}
