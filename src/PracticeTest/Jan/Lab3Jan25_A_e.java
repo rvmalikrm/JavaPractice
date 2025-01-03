a0a69453add1c911eb8c52ea17b9555511b6cb1e
rvmalikrm <rm.rvmalik@gmail.com>
2025-01-03 17:11:53 +0530

    Commit for data type Lab3Jan25_A_e

---
+++ src/PracticeTest/Jan/Lab3Jan25_A_e.java
@@ -0,0 +1,13 @@
+package PracticeTest.Jan;
+
+public class Lab3Jan25_A_e {
+    public static void main(String[] args) {
+
+        float a = 2.2f, b = 0.0f, c = 4.1f, y = 3.0f;
+        float t;
+        t = c + a * y * y / b ;
+        //(a = 2.2, b = 0.0, c = 4.1, y = 3.0, assume t to be an float)
+
+        System.out.println((int)t);
+    }
+}
