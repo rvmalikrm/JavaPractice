dad9e911e88cffd2f17524864e04f9cd9a29a248
rvmalikrm <rm.rvmalik@gmail.com>
2025-01-03 16:57:32 +0530

    Lab3Jan25_A_a commit for data type

---
+++ src/PracticeTest/Jan/Lab3Jan25_A_a.java
@@ -0,0 +1,11 @@
+package PracticeTest.Jan;
+
+public class Lab3Jan25_A_a {
+    public static void main(String[] args) {
+        float abc=2.5f;
+        int big = 2;
+        float g;
+        g= big/2+big*4/big-big+abc/3;
+        System.out.println((int)g);
+    }
+}
