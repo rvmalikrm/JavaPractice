d7191a5153ac2b5343fda6044c61b24dcf9e44fe
rvmalikrm <rm.rvmalik@gmail.com>
2025-01-20 12:24:48 +0530

    Commmit for 20 Jan for Student division example 4_2

---
+++ src/PracticeTest/Jan/Lab20Jan_Example_4_2.java
@@ -0,0 +1,25 @@
+package PracticeTest.Jan;
+
+import java.util.Scanner;
+
+public class Lab20Jan_Example_4_2 {
+    public static void main(String[] args) {
+            float sub1, sub2, sub3, Total;
+            Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the student's all subject mark");
+        sub1=sc.nextFloat();
+        sub2=sc.nextFloat();
+        sub3=sc.nextFloat();
+
+        Total=sub1+sub2+sub3;
+        Float percentage= (Total*100)/300;
+        if(percentage>=60)
+            System.out.println("The student secured First division ");
+        if((percentage>50) && (percentage<60))
+            System.out.println("The student secured Second division ");
+        if((percentage>40)&&(percentage<50))
+            System.out.println("The student secured Third division ");
+        if(percentage<40)
+            System.out.println("Student FAILED");
+    }
+}
