d8e563e9292bd4ea208aab7ae58cf3650032de23
rvmalikrm <rm.rvmalik@gmail.com>
2025-01-21 17:31:33 +0530

    Commmit for 21Jan25 A_d

---
+++ src/PracticeTest/Jan/Lab21Jan25_A_d.java
@@ -0,0 +1,9 @@
+package PracticeTest.Jan;
+
+public class Lab21Jan25_A_d {
+    public static void main(String[] args) {
+        int k, num=3;
+        k=(num>=5?(num<=10?100:200):500);
+        System.out.println(k);
+    }
+}
