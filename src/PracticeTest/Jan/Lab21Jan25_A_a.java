93b34487ceec041eb667fb37d867dc5831b4025b
rvmalikrm <rm.rvmalik@gmail.com>
2025-01-21 15:58:15 +0530

    Commmit for 21Jan25 A_b

---
+++ src/PracticeTest/Jan/Lab21Jan25_A_a.java
@@ -0,0 +1,12 @@
+package PracticeTest.Jan;
+
+public class Lab21Jan25_A_a {
+    public static void main(String[] args) {
+        int a=300, b=0, c;
+        if(a>=400)
+            b=300;
+            c=200;
+        System.out.println(b + " " + c);
+
+    }
+}
---
+++ src/PracticeTest/Jan/Lab21Jan25_A_b.java
@@ -0,0 +1,10 @@
+package PracticeTest.Jan;
+
+public class Lab21Jan25_A_b {
+    public static void main(String[] args) {
+       int x=10, y=20;
+       if(x==y)
+        System.out.println(x+ " First Line" +y);
+        System.out.println(x+ " Second Line " +y);
+    }
+}
