1709018124da2153d3d80d6bf268380b2888e480
rvmalikrm <rm.rvmalik@gmail.com>
2025-01-21 16:08:28 +0530

    Commmit for 21Jan25 A_c

---
+++ src/PracticeTest/Jan/Lab21Jan25_A_c.java
@@ -0,0 +1,9 @@
+package PracticeTest.Jan;
+
+public class Lab21Jan25_A_c {
+    public static void main(String[] args) {
+        int i = 4, num = -4, j;
+        j = i < 0 ? 0 : num * num;
+        System.out.println(j);
+    }
+}
