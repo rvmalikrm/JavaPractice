09f79c43ecc7ba67d8f1420025fd4a46d1fb123d
rvmalikrm <rm.rvmalik@gmail.com>
2025-01-22 14:03:55 +0530

    Commmit for 22 Jan B_c

---
+++ src/PracticeTest/Jan/Lab22Jan25_B_c.java
@@ -0,0 +1,9 @@
+package PracticeTest.Jan;
+
+public class Lab22Jan25_B_c {
+    public static void main(String[] args) {
+        char spy='a', password='z';
+        if(spy=='a' | password=='z')
+        System.out.println("All birds are safe in the nest");
+    }
+}
