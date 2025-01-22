3faa549e4f5ee305b3e9132c9e8802f05ceed91d
rvmalikrm <rm.rvmalik@gmail.com>
2025-01-22 14:00:03 +0530

    Commmit for 22 Jan B_b

---
+++ src/PracticeTest/Jan/Lab22Jan25_B_b.java
@@ -0,0 +1,9 @@
+package PracticeTest.Jan;
+
+public class Lab22Jan25_B_b {
+    public static void main(String[] args) {
+        int x=10, y=15;
+        if(x%2==y%3)
+            System.out.println("Carpathians");
+    }
+}
