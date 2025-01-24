8677b3d77be9ddfe4486edc8075543ca4c923693
rvmalikrm <rm.rvmalik@gmail.com>
2025-01-24 11:30:18 +0530

    Commmit for 24 jan Array

---
+++ src/PracticeTest/Jan/Lab24Jan25_Arrayusingforloop.java
@@ -0,0 +1,16 @@
+package PracticeTest.Jan;
+
+public class Lab24Jan25_Arrayusingforloop {
+    public static void main(String[] args) {
+        int[] a= new int[5];
+        a[0]=9;
+        a[1]=8;
+        a[2]=7;
+        a[3]=5;
+        a[4]=3;
+        for(int i=0;i<a.length;i++)
+        {
+            System.out.println(a[i]);
+        }
+    }
+}
