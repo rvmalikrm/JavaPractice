f681939893486acc05fd2fa3f22b0fc4107db87f
rvmalikrm <rm.rvmalik@gmail.com>
2025-01-27 15:48:03 +0530

    Commmit for 27 Jan25 For Loop_p2 and _p3

---
+++ src/PracticeTest/Jan/Lab27Jan25_For_loop_p2.java
@@ -0,0 +1,11 @@
+package PracticeTest.Jan;
+
+import java.util.Scanner;
+
+public class Lab27Jan25_For_loop_p2 {
+    public static void main(String[] args) {
+        for(int i=0;i<=10; System.out.println(i++))
+            ;
+        }
+    }
+
---
+++ src/PracticeTest/Jan/Lab27Jan25_For_loop_p3.java
@@ -0,0 +1,13 @@
+package PracticeTest.Jan;
+
+import java.util.Scanner;
+
+public class Lab27Jan25_For_loop_p3 {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the number that you want to start for loop");
+        for(int i=sc.nextInt();i<=10;i++)
+            System.out.println(i);
+        }
+    }
+
