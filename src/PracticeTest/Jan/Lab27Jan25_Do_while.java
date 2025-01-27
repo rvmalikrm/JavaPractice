c2b3fcd6a7bfd4caaf2dcc2ce7fc2dae9d6cd6be
rvmalikrm <rm.rvmalik@gmail.com>
2025-01-27 18:41:06 +0530

    Commmit for 27 Jan25 Reverse the String

---
+++ src/PracticeTest/Jan/Lab27Jan25_Do_while.java
@@ -0,0 +1,14 @@
+package PracticeTest.Jan;
+/****************************************************************************
+ *                                                                          *
+ *          Program Name: Comparing Area and Perimeter of Rectangle         *
+ * -----------------------------------------------------------------------  *
+ * Description:           do While Loop                                     *
+ * /************************************************************************/
+
+public class Lab27Jan25_Do_while {
+
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/PracticeTest/Jan/Lab27Jan25_Reverse_the_String.java
@@ -0,0 +1,11 @@
+package PracticeTest.Jan;
+
+public class Lab27Jan25_Reverse_the_String {
+
+    public static void main(String[] args) {
+        String s="Ravi Malik";
+        for(int i=s.length()-1;i>=0;i--){
+            System.out.println(s.charAt(i));
+        }
+    }
+}
