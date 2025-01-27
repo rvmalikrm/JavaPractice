4661ca9921fb2635a8bba7de8cd26ec1f6e210d1
rvmalikrm <rm.rvmalik@gmail.com>
2025-01-27 20:15:56 +0530

    Commmit for 27 Jan25 chapter_A_e

---
+++ src/PracticeTest/Jan/Lab27Jan25_Chapter_5_A_e.java
@@ -0,0 +1,14 @@
+package PracticeTest.Jan;
+
+public class Lab27Jan25_Chapter_5_A_e {
+    public static void main(String[] args)
+            {
+                int i ;
+                for ( i = 1 ; i <= 5 ; System.out.println ( i ) )
+                i++ ;
+            }
+
+}
+
+
+
