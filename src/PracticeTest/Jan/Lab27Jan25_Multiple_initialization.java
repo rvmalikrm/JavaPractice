409a002672ca88455274f8cd520070c4c341ff58
rvmalikrm <rm.rvmalik@gmail.com>
2025-01-27 17:52:30 +0530

    Commmit for 27 Jan25 nested for Loop_p2

---
+++ src/PracticeTest/Jan/Lab27Jan25_Multiple_initialization.java
@@ -0,0 +1,10 @@
+package PracticeTest.Jan;
+
+public class Lab27Jan25_Multiple_initialization {
+    public static void main(String[] args) {
+        for (int i = 0, j = 100; i <= j; i++, j--) {
+            System.out.println("i = " + i + ", j = " + j);
+        }
+
+    }
+}
