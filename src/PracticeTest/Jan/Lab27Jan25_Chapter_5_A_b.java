2c52ed700e58ca602792d0989a5f5a8eda9d7979
rvmalikrm <rm.rvmalik@gmail.com>
2025-01-27 20:01:19 +0530

    Commmit for 27 Jan25 chapter_A_c

---
+++ src/PracticeTest/Jan/Lab27Jan25_Chapter_5_A_b.java
@@ -0,0 +1,10 @@
+package PracticeTest.Jan;
+
+public class Lab27Jan25_Chapter_5_A_b {
+    public static void main(String[] args) {
+        int x=4,y=3,z;
+        z=x-- -y;
+        System.out.println(x+""+y+""+z);
+        }
+    }
+
