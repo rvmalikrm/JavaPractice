93a9d589b70b80df1887039ac8e7f30442416b74
rvmalikrm <rm.rvmalik@gmail.com>
2025-01-27 18:45:55 +0530

    Commmit for 27 Jan25 do while loop

--- src/PracticeTest/Jan/Lab27Jan25_Do_while.java
+++ src/PracticeTest/Jan/Lab27Jan25_Do_while.java
@@ -9,6 +9,12 @@ package PracticeTest.Jan;
 public class Lab27Jan25_Do_while {
 
     public static void main(String[] args) {
+        int i=1;
+        do{
+            System.out.println(i);
+            i++;
+        }        while(i<3);
+
 
     }
 }
