b828ac052cc737d217ce32784320469feaa045e2
rvmalikrm <rm.rvmalik@gmail.com>
2025-01-27 20:13:06 +0530

    Commmit for 27 Jan25 chapter_A_d

---
+++ src/PracticeTest/Jan/Lab27Jan25_Chapter_5_A_d.java
@@ -0,0 +1,26 @@
+package PracticeTest.Jan;
+
+public class Lab27Jan25_Chapter_5_A_d {
+    public static void main(String[] args) {
+        int x=4,y=0,z;
+        while(x>=0)
+             {
+                if(x==y)
+                {
+                    System.out.println("x and y are same");
+                    x--;
+                    y++;
+                    continue;
+                }
+                 else
+                 {
+                    System.out.println(x + "" + y);
+                    x--;
+                    y++;
+                }
+            }
+
+        }
+        }
+
+
