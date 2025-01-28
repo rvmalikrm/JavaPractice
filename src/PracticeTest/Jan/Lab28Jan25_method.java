db73f0ce2e81f433df32462e93690ec3fe6efb05
rvmalikrm <rm.rvmalik@gmail.com>
2025-01-28 11:40:52 +0530

    Commmit for 28Jan25 methods

---
+++ src/PracticeTest/Jan/Lab28Jan25_method.java
@@ -0,0 +1,14 @@
+package PracticeTest.Jan;
+
+public class Lab28Jan25_method {
+    public static void main(String[] args) {
+    Lab28Jan25_method obj=new Lab28Jan25_method();
+    String name =obj.getData();
+        System.out.println(name);
+
+    }
+    public String getData(){
+        System.out.println("Enter the into the Selenium Automation Workd");
+        return "Ravi Malik";
+    }
+}
