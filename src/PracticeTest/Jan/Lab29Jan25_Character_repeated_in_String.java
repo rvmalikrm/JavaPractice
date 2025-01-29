90aef9d2d640c9ddb90315342e844621dcc8fc8b
rvmalikrm <rm.rvmalik@gmail.com>
2025-01-29 19:02:45 +0530

    Commmit for 29jan25 count of character

--- src/PracticeTest/Jan/Lab29Jan25_Character_repeated_in_String.java
+++ src/PracticeTest/Jan/Lab29Jan25_Character_repeated_in_String.java
@@ -4,20 +4,25 @@ import java.util.Arrays;
 
 public class Lab29Jan25_Character_repeated_in_String {
     public static void main(String[] args) {
-        String s="abbccc";
+        String s="aabccc";
+
+
 
         for(int i=0;i<s.length()-1;i++) {
-            System.out.println(s.length());
-        int count=0;
+            for(int j=0;j<s.length();j++){
+            System.out.println("numbers of char "+s.length());
+            int count=0;
 
-            char ch=s.charAt(1);
-            if(ch==s.charAt(i))
+            char ch=s.charAt(i);
+
+            if(ch==s.charAt(j))
                 count=count+1;
             System.out.println("count of  "+i+" is "+count);
+            System.out.println("count of  "+j+" is here "+count);
 
 
             System.out.println(ch);
         }
-
+            System.out.println("count for "+i+"is");
     }
-}
+}}
