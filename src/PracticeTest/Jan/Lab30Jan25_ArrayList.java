beff2770011abf2e3a6902b355843cc4ad502346
rvmalikrm <rm.rvmalik@gmail.com>
2025-01-30 12:53:07 +0530

    Commmit for 30jan25 ArrayListExample

---
+++ src/PracticeTest/Jan/Lab30Jan25_ArrayList.java
@@ -0,0 +1,19 @@
+package PracticeTest.Jan;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Lab30Jan25_ArrayList {
+    public static void main(String[] args) {
+        List<String> list=new ArrayList<>();
+        list.add("apple");
+        list.add("Orange");
+        list.add("Banana");
+        list.add("Cherry");
+        list.add("Banana");
+        list.add("Grapes");
+        list.add("Pomogranate");
+        list.add("Banana");
+        System.out.println(list);
+    }
+}
