7c1b46673de9cf49831b3f7ed7957956707b3304
rvmalikrm <rm.rvmalik@gmail.com>
2025-01-30 13:11:09 +0530

    Commmit for 30jan25 Map_sorting_using the colletction

---
+++ src/PracticeTest/Jan/Lab30Jan25_map_sorting.java
@@ -0,0 +1,13 @@
+package PracticeTest.Jan;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+public class Lab30Jan25_map_sorting {
+    public static void main(String[] args) {
+        List<Integer> list= Arrays.asList(3,5,1,7);
+        Collections.sort(list);
+        System.out.println(list);
+    }
+}
