edadf6cfd4fab9c0761b0bf9ecd52cf07c7559e3
rvmalikrm <rm.rvmalik@gmail.com>
2025-02-03 17:38:31 +0530

    Commmit for Function Chatper 7 fucntions_p2

---
+++ src/PracticeTest/Feb/Lab03Feb25_Functions_p2.java
@@ -0,0 +1,25 @@
+package PracticeTest.Feb;
+
+public class Lab03Feb25_Functions_p2 {
+    public static void main(String[] args) {
+
+        System.out.println("I am the Main");
+        Lab03Feb25_Functions_p2.Italy();
+        Lab03Feb25_Functions_p2.Japan();
+        Lab03Feb25_Functions_p2.Brazil();
+        Lab03Feb25_Functions_p2.Argentina();
+        Lab03Feb25_Functions_p2.India();
+
+    }
+    static void Italy(){
+        System.out.println("I am in Italy");
+    }static void Japan(){
+        System.out.println("I am in Japan");
+    }static void Brazil(){
+        System.out.println("I am in Brazil");
+    }static void Argentina(){
+        System.out.println("I am in Argentina");
+    }static void India(){
+        System.out.println("I am in India");
+    }
+}
