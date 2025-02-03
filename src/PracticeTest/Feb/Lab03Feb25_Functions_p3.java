b0c0ab47d0cdba67946e597a04a8421cd184a11a
rvmalikrm <rm.rvmalik@gmail.com>
2025-02-03 19:50:04 +0530

    Commmit for Function Chatper 7 fucntions_p3

---
+++ src/PracticeTest/Feb/Lab03Feb25_Functions_p3.java
@@ -0,0 +1,23 @@
+package PracticeTest.Feb;
+
+public class Lab03Feb25_Functions_p3 {
+    public static void main(String[] args) {
+        System.out.println("I am in main");
+        Italy();
+        System.out.println("I am finally back in main");
+
+    }
+    static void Italy(){
+        System.out.println("I am in Italy");
+        Brazil();
+        System.out.println("I am back in Italy");
+    }
+    static void Brazil(){
+        System.out.println("I am in brazil");
+    argentina();
+    }
+    static  void argentina(){
+        System.out.println(" I am in argentina");
+    }
+
+}
