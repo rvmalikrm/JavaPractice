690ea37f7a0ec2986f28cfe43b49e96df62b105a
rvmalikrm <rm.rvmalik@gmail.com>
2025-02-05 17:29:56 +0530

    Commmit for Lab03Feb25_Chap_7_B_a

---
+++ src/PracticeTest/Feb/Lab05Feb25_B_b.java
@@ -0,0 +1,11 @@
+package PracticeTest.Feb;
+
+public class Lab05Feb25_B_b {
+    public static void main(String[] args) {
+        message();
+        message();
+    }
+    public static void message(){
+        System.out.println("Praised woorthy effort");
+    }
+}
