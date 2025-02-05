ffcedcb8e0a1b55c8a577c336082d02dd81b4d5d
rvmalikrm <rm.rvmalik@gmail.com>
2025-02-05 17:27:43 +0530

    Commmit for Lab03Feb25_Chap_7_B_a

---
+++ src/PracticeTest/Feb/Lab03Feb25_Chap_7_B_a.java
@@ -0,0 +1,17 @@
+package PracticeTest.Feb;
+
+public class Lab03Feb25_Chap_7_B_a {
+    public static void main(String[] args) {
+        int i=3, j=4, k,l;
+        k=addMult(i,j);
+        l=addMult(i,j);
+        System.out.println(k+" "+l);
+    }
+    static int addMult(int ii,int jj){
+        int kk,ll;
+        kk=ii+jj;
+        ll=ii*jj;
+        return (ll);
+
+    }
+}
