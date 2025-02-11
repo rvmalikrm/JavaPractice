6ffc9c7259e9105850f2387893a9fdef5997bab7
rvmalikrm <rm.rvmalik@gmail.com>
2025-02-11 17:53:44 +0530

    Commmit for Quiz_Me_reverse the world of String

--- src/PracticeTest/Feb/Lab06Feb25_QuizMe_reversestring.java
+++ src/PracticeTest/Feb/Lab06Feb25_QuizMe_reversestring.java
@@ -1,27 +1,38 @@
 package PracticeTest.Feb;
 import java.util.List;
 import java.util.ArrayList;
+import java.util.Scanner;
 
-public class Lab06Feb25_QuizMe_reversestring {
-    public static void main(String[] args) {
-        String input = "Hello World!";
-        ArrayList[] c = new ArrayList[input.length()];
-        String str1 = input.substring(0, 5);
-        String str2 = input.substring(6, 11);
-        String rstr1;
-        System.out.println(str1);
-        System.out.println(str2);
-        for (int i = str1.length() - 1; i >= 0; i--)
+public class Lab06Feb25_QuizMe_reversestring
+{
+    public static void main(String[] args)
+    {
+//        Scanner sc=new Scanner(System.in);
+//        System.out.println("Enter the String");
+//        String input=sc.next();
+        String input="Hello World!";
+        int start=0;
+        String reverse="";
+        for(int i=0;i<=input.length();i++)
+        {
+            if(i==input.length() || input.charAt(i)==' ')
             {
-                char ch = str1.charAt(i);
-                System.out.print(ch);
-                //System.out.println(c);
-            }
-            System.out.println(" ");
-            for (int j = str2.length() - 1; j >= 0; j--) {
-                char ch = str2.charAt(j);
-                System.out.print(ch);
-            }
+                for(int j=i-1;j>=start;j--)
+                {
+                    reverse = reverse + input.charAt(j);
+                }
+                    if(i!=input.length())
+                    {
+                        reverse=reverse+" ";
+                    }
+                start=i+1;
 
-    }
-}
+                }
+
+            }
+        System.out.println(reverse);
+        }
+
+        }
+
+
