a9275d2754ffc980990291bdc7ec588c9358d910
rvmalikrm <rm.rvmalik@gmail.com>
2025-03-23 22:41:07 +0530

    Commit for Prevent_Inheritance_Project

---
+++ src/PracticeTest/Mar/Lab21Mar25_Abstraction.java
@@ -0,0 +1,8 @@
+package PracticeTest.Mar;
+
+
+public class Lab21Mar25_Abstraction {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/PracticeTest/Mar/Lab23MAr25_order_of_construction2.java
@@ -0,0 +1,18 @@
+package PracticeTest.Mar;
+class base
+    protected int i;
+    public base(){
+        i=4;
+    }
+class derrived extends base
+{
+    private int r;
+    public derrived()
+    {
+     r=i*5;
+}
+public class Lab23MAr25_order_of_construction2 {
+    public static void main(String[] args) {
+        derrived d=new derrived();
+    }
+    }
