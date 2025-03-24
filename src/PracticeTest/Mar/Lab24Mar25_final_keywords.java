64ddab21a759935a3573d8165ee9c1d41e2c2bb4
rvmalikrm <rm.rvmalik@gmail.com>
2025-03-24 23:35:25 +0530

    Commit for final keyword

---
+++ src/PracticeTest/Mar/Lab24Mar25_final_keywords.java
@@ -0,0 +1,17 @@
+package PracticeTest.Mar;
+final class base
+{
+
+}
+class der extends base
+public void fun(){
+    System.out.println("There is so much noise, small substance");
+}
+
+public class Lab24Mar25_final_keywords {
+    public static void main(String[] args) {
+        der obj=new der();
+        d.fun();
+
+    }
+}
