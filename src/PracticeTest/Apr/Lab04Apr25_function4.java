f65b2cd9ad37ec20fefc48251e70070377e4bc70
rvmalikrm <rm.rvmalik@gmail.com>
2025-04-04 10:38:21 +0530

    Commit funtion without pare and return

--- src/PracticeTest/Apr/Lab04Apr25_function4.java
+++ src/PracticeTest/Apr/Lab04Apr25_function4.java
@@ -8,11 +8,11 @@ public class Lab04Apr25_function4 {
         // 2. Without Parameters but with Return Type
         // 3. With Paremeters and without Return Type
         // 4. With Parameters and with Return Type
-
-        
-
-
-
+        for (int i=0;i<100;i++)
+        greet();
+    }
+    static void greet(){
+        System.out.println("Hi, How are your");
 
     }
 
