8cca633711f8fa2e734c0d4a9b87d8b9ee5b733d
rvmalikrm <rm.rvmalik@gmail.com>
2025-04-04 10:35:11 +0530

    Commit funtion built in and user

---
+++ src/PracticeTest/Apr/Lab04Apr25_function4.java
@@ -0,0 +1,19 @@
+package PracticeTest.Apr;
+
+public class Lab04Apr25_function4 {
+    public static void main(String[] args) {
+        // user defined function
+
+        // 1. Without Parameters and Without Return Type
+        // 2. Without Parameters but with Return Type
+        // 3. With Paremeters and without Return Type
+        // 4. With Parameters and with Return Type
+
+        
+
+
+
+
+    }
+
+}
--- src/PracticeTest/Apr/Lab04Apr25_funtion2.java
+++ src/PracticeTest/Apr/Lab04Apr25_funtion2.java
@@ -4,7 +4,8 @@ public class Lab04Apr25_funtion2 {
     public static void main(String[] args) {
         int result= Math.max(3, 4);
         System.out.println(result);
-        Math.
+        double L=Math.min(4.5,5.4);
+        System.out.println(L);
 
 
     }
