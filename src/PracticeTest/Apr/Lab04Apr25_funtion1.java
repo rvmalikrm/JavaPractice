4224c713b5ec4f5d5102e445fe28d6801c4784d3
rvmalikrm <rm.rvmalik@gmail.com>
2025-04-04 10:18:52 +0530

    Commit funtion

---
+++ src/PracticeTest/Apr/Lab04Apr25_funtion1.java
@@ -0,0 +1,14 @@
+package PracticeTest.Apr;
+
+public class Lab04Apr25_funtion1 {
+    public static void main(String[] args) {
+        System.out.println("Hi , How are you");
+        greet();
+        greet();
+        greet();
+
+    }
+    static void greet(){
+        System.out.println("Greet");
+    }
+}
