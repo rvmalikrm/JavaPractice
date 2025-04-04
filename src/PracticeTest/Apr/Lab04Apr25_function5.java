809c59b23bb975d40cd923109f6cec1dd1f51484
rvmalikrm <rm.rvmalik@gmail.com>
2025-04-04 12:06:05 +0530

    Commit function main with different typo

---
+++ src/PracticeTest/Apr/Lab04Apr25_function5.java
@@ -0,0 +1,23 @@
+package PracticeTest.Apr;
+
+public class Lab04Apr25_function5 {
+    public static void main(String[] args) {
+
+    }
+
+    public static void main(int a) {
+
+    }
+
+    public static void main(String args) {
+
+    }
+
+    public static int main() {
+        return 34;
+    }
+
+    public static String main(String a, String b) {
+        return a + b;
+    }
+}
