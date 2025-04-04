9534c24d8986f95b35e16de66ae74ec3cb4e1cf8
rvmalikrm <rm.rvmalik@gmail.com>
2025-04-04 10:25:55 +0530

    Commit funtion built in and user define

---
+++ src/PracticeTest/Apr/Lab04Apr25_function3.java
@@ -0,0 +1,21 @@
+package PracticeTest.Apr;
+
+public class Lab04Apr25_function3 {
+    public static void main(String[] args) {
+        // Functions
+        // Block of code which we can reuse
+
+        // 1. Build IN Math.max. min ... String... time. Date..
+        // 2. User defined function
+
+        // Two Component of the functions
+
+        // 1. Define/ Declare
+        // 2. Call the function
+
+
+
+
+    }
+
+}
--- src/PracticeTest/Apr/Lab04Apr25_funtion2.java
+++ src/PracticeTest/Apr/Lab04Apr25_funtion2.java
@@ -4,6 +4,7 @@ public class Lab04Apr25_funtion2 {
     public static void main(String[] args) {
         int result= Math.max(3, 4);
         System.out.println(result);
+        Math.
 
 
     }
