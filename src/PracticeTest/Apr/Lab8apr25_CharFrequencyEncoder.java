5ec721707a9ffe15e82a8858511ed2c35f266c55
rvmalikrm <rm.rvmalik@gmail.com>
2025-04-11 12:33:00 +0530

    Commit for Chareacter Frequency in String

---
+++ src/PracticeTest/Apr/Lab8apr25_CharFrequencyEncoder.java
@@ -0,0 +1,22 @@
+package PracticeTest.Apr;
+//import java.lang.String.*;
+import java.util.*;
+public class Lab8apr25_CharFrequencyEncoder
+{
+    public static void main(String[] args)
+    {
+        String input="aabbbccdda";
+        String output =EncoderCharacterFrequency(input);
+        System.out.println(output);
+    }
+    public static String EncoderCharacterFrequency(String str)
+    {
+        Map<Character, Integer> countMap = new LinkedHashMap<Character, Integer>();
+        StringBuilder sb = new StringBuilder();
+        for (char ch : str.toCharArray())
+        {
+            countMap.put(ch, countMap.getOrDefault(ch, 0) + 1);
+        }
+        return countMap.toString();
+    }
+}
