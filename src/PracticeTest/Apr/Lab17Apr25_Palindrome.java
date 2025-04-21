df92be518ca6e0be148e51291f2e27c84b97260a
rvmalikrm <rm.rvmalik@gmail.com>
2025-04-21 19:42:48 +0530

    Commit for Palindrome

--- src/PracticeTest/Apr/Lab17Apr25_Palindrome.java
+++ src/PracticeTest/Apr/Lab17Apr25_Palindrome.java
@@ -1,24 +1,19 @@
 package PracticeTest.Apr;
+public class Lab17Apr25_Palindrome {
+    public static void main(String[] arg) {
+        String input = "MAdam";
 
-public class Lab17Apr25_Palindrome
-{
-    public static void main(String[] arg)
-    {
-            String input = "Madam";
-            if (isPalindrome(input.toLowerCase()))
-            {
-                System.out.println("Palindorme");
-            } else
-            {
-                System.out.println("Not palindrome");
-            }
+        if (isPalindrome(input.toLowerCase())) {
+            System.out.println("Palindorme");
+        } else {
+            System.out.println("Not palindrome");
+        }
+
     }
-    public static boolean isPalindrome(String str){
-        for(int i=0;i<str.length()/2;i++)
-        {
-            if(str.charAt(i)!=str.charAt(str.length()-i-1))
-            {
-                System.out.println("Not Palindorme");
+    public static boolean isPalindrome(String str) {
+//        str.replaceAll("[^a-zA-Z0-9]","");
+        for (int i = 0; i < str.length() / 2; i++) {
+            if (str.charAt(i) != str.charAt(str.length() - i - 1)) {
                 return false;
             }
         }
