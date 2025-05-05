00f054f6a70457948b3b7107ae966ad39d0ef111
rvmalikrm <rm.rvmalik@gmail.com>
2025-05-05 18:07:02 +0530

    Commit for LinkedList Demo

---
+++ src/PracticeTest/May/Lab5May25_Linked_list_demi.java
@@ -0,0 +1,29 @@
+package PracticeTest.May;
+
+import java.util.LinkedList;
+
+public class Lab5May25_Linked_list_demi
+{
+    public static void main(String[] args)
+    {
+        LinkedList<String> ll=new LinkedList<>();
+        ll.add("Subhash");
+        ll.add("Rahul");
+        ll.add("Joe");
+        ll.add("Vineeta");
+        for (String s: ll)
+        {
+            System.out.println(s);
+        }
+        ll.set(2, "Neha");
+            System.out.println(ll);
+        String name =ll.get(2);
+            System.out.println("String at position 2 "+name);
+        ll.add(3, "Sanjay");
+            System.out.println(ll);
+        ll.remove(1);
+            System.out.println(ll);
+
+
+    }
+}
