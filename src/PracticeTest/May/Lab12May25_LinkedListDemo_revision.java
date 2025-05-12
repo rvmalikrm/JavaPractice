ea7b1f8b2bfcd5432187152100f4d72a2963eb91
rvmalikrm <rm.rvmalik@gmail.com>
2025-05-12 18:52:36 +0530

    Commit for 12 may LinkedListDemo_revision

---
+++ src/PracticeTest/May/Lab12May25_LinkedListDemo_revision.java
@@ -0,0 +1,21 @@
+package PracticeTest.May;
+
+import java.util.LinkedList;
+
+public class Lab12May25_LinkedListDemo_revision {
+    public static void main(String[] args) {
+
+
+        LinkedList<String> ll = new LinkedList<>();
+        ll.add("Dog");
+        ll.add("Vegitable");
+        ll.add("Fruits");
+        ll.add("Pulse");
+        ll.add("Rice");
+        ll.add("Cane");
+        for (String s : ll)
+            System.out.print(s);
+
+
+    }
+}
