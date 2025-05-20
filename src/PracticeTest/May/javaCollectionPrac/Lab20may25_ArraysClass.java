052658090d14b8aa2cbb677520df6e5921bc18f6
rvmalikrm <rm.rvmalik@gmail.com>
2025-05-20 17:57:55 +0530

    Commit for Arrays operations

--- src/PracticeTest/May/javaCollectionPrac/Lab20may25_ArraysClass.java
+++ src/PracticeTest/May/javaCollectionPrac/Lab20may25_ArraysClass.java
@@ -31,5 +31,29 @@ public class Lab20may25_ArraysClass {
         int[] aa = {10, 20, 30, 40, 50};
         int[] sub= Arrays.copyOfRange(aa, 1,3);
         System.out.println(Arrays.toString(sub));
+
+        // Arrays.fill(array, value)
+
+        int[] arr2 = new int[5];
+        Arrays.fill(arr2, 10);
+        System.out.println(Arrays.toString(arr2));
+
+        //Arrays.binarySearch(array, value)
+
+        int[] aaa = {1, 3, 5, 7, 9};
+        int index= Arrays.binarySearch(aaa,7);
+        System.out.println(index);
+
+        //Arrays.deepEquals(arr1, arr2) to compare multi dimentional array
+
+        int[][] a3 = {{1,2}, {3,4}};
+        int[][] b3 = {{1,2}, {3,4}};
+        System.out.println(Arrays.deepEquals(a3,b3));
+
+        //Arrays.deepToString(array)
+
+        int[][] matrix = {{1, 2}, {3, 4}};
+        System.out.println(Arrays.deepToString(matrix));
+
     }
 }
