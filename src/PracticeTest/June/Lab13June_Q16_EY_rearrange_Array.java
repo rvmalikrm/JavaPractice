15fd193e9e1a5839ec98767a078126ce245cdf61
rvmalikrm <rm.rvmalik@gmail.com>
2025-06-13 12:08:34 +0530

    commit for Ques EYY

---
+++ src/PracticeTest/June/Lab13June_Q16_EY_rearrange_Array.java
@@ -0,0 +1,12 @@
+package PracticeTest.June;
+
+public class Lab13June_Q16_EY_rearrange_Array {
+
+    public static void main(String[] args) {
+        int[] nums= {1,2,6,8};
+        int[] output = checkmul(input);
+    }
+    public static int[] checkmul(int[] nums){
+        for
+    }
+}
