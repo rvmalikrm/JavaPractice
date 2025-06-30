bb11c99526404e148d60cc5b11b612dfa5d3798a
rvmalikrm <rm.rvmalik@gmail.com>
2025-06-30 16:13:13 +0530

    commit for Ques EYY

---
+++ src/PracticeTest/June/Lab05June25_Que5_String_Contains_only_Digit.java
@@ -0,0 +1,23 @@
+package PracticeTest.June;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Lab05June25_Que5_String_Contains_only_Digit {
+    public static void main(String[] args) {
+        String input= "12456789";
+        boolean output= digitChecker(input);
+        System.out.println(output);
+    }
+    public  static boolean digitChecker(String str)
+    {
+        boolean onlydigit = true;
+        char[] chararray = str.toCharArray();
+        for (char ch : chararray)
+            if (!Character.isDigit(ch)) {
+                onlydigit = false;
+                break;
+            }
+    return onlydigit;
+    }
+}
