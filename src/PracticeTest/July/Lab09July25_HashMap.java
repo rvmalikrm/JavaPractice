2bc9958b25b27b3d7c5aa4393edaa975398272fd
rvmalikrm <rm.rvmalik@gmail.com>
2025-07-09 15:46:58 +0530

    commit for Questtion Find key of the Value in HashMap

---
+++ src/PracticeTest/July/Lab09July25_HashMap.java
@@ -0,0 +1,18 @@
+package PracticeTest.July;
+import java.util.HashMap;
+import java.util.Map;
+public class Lab09July25_HashMap {
+    public static void main(String[] args) {
+        Map<Integer, String> map = new HashMap<>();
+        map.put(1, "Rakesh");
+        map.put(2, "Mahesh");
+        map.put(3, "Suresh");
+        map.put(4, "Akansha");
+        for (Map.Entry<Integer, String> entry : map.entrySet()) {
+            if (entry.getValue() == "Suresh") {
+                System.out.println("Key is --> "+entry.getKey());
+            }
+        }
+    }
+}
+
