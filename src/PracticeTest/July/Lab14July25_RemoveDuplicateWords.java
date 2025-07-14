5c851e8593fdb47ffbcb6fe691a364f370a530a0
rvmalikrm <rm.rvmalik@gmail.com>
2025-07-14 17:35:04 +0530

    commit for Question remove duplicate

--- src/PracticeTest/July/Lab14July25_RemoveDuplicateWords.java
+++ src/PracticeTest/July/Lab14July25_RemoveDuplicateWords.java
@@ -5,18 +5,23 @@ public class Lab14July25_RemoveDuplicateWords
 {
     public static void main(String[] args)
     {
-        String input="mk ne mk ko mk se maar dala";
+        String input="mk ne mk ko maar se dala mk se maar dala";
         String[] words= input.split(" ");
         Set<String> uniquewords= new LinkedHashSet<>();
+        Set<String> duplicate= new LinkedHashSet<>();
         for(String word: words)
+        if(!uniquewords.add(word))
         {
-            uniquewords.add(word);
+            duplicate.add(word);
         }
-        StringBuilder sb= new StringBuilder();
-        for(String word: uniquewords)
-        {
-            sb.append(word).append(" ");
+//        StringBuilder sb= new StringBuilder();
+//        for(String word: uniquewords)
+//        {
+//            sb.append(word).append(" ");
+//        }
+        //System.out.println(sb.toString().trim());
+            System.out.println(duplicate+ " ");
         }
-        System.out.println(sb.toString().trim());
+
     }
-}
+
