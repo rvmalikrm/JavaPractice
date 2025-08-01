9eed2288c5f8a87e9a2f927a8b7eaf3ac789c851
rvmalikrm <rm.rvmalik@gmail.com>
2025-08-01 13:26:34 +0530

    commit for String Variations

---
+++ src/PracticeTest/Aug/Ques01.java
@@ -0,0 +1,16 @@
+package PracticeTest.Aug;
+
+public class Ques01
+{
+        static int index = 0;
+        public static void main(String args[])
+        {
+            System.out.println(test());
+        }
+        static int test()
+        {
+            int index = 1;
+            return index;
+        }
+    }
+
---
+++ src/PracticeTest/Aug/stringLen.java
@@ -0,0 +1,11 @@
+package PracticeTest.Aug;
+
+public class stringLen {
+    static String[] input= {"1","2"};
+
+    public static void main(String[] args) {
+        if(input.length>0)
+            System.out.println(input.length);
+    }
+
+}
