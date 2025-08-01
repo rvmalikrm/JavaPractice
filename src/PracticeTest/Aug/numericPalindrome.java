d88e0b70064a344e95a255b9cb17acd9df813ab6
rvmalikrm <rm.rvmalik@gmail.com>
2025-08-01 12:48:58 +0530

    commit for Palindrome of Number

---
+++ src/PracticeTest/Aug/numericPalindrome.java
@@ -0,0 +1,16 @@
+package PracticeTest.Aug;
+public class numericPalindrome
+{
+    public static void main(String[] args)
+    {
+        int num = 12321;
+        boolean result =isNumPalindrome(num);
+        System.out.println("Given number is Palindrome -->  "+result);
+    }
+    public static boolean isNumPalindrome(int num)
+    {
+        String input = String.valueOf(num);
+        String reverseString= new StringBuilder(input).reverse().toString();
+        return (input.equals(reverseString));
+    }
+}
