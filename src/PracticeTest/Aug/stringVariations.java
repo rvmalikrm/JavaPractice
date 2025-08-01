1047af64e5a7961f54ad07693d2647a5fedd62c8
rvmalikrm <rm.rvmalik@gmail.com>
2025-08-01 12:58:14 +0530

    commit for String Variations

---
+++ src/PracticeTest/Aug/stringVariations.java
@@ -0,0 +1,9 @@
+package PracticeTest.Aug;
+
+public class stringVariations {
+    public static void main(String[] args) {
+        String s1= "Hello I love India";
+        String s2= new String(s1);
+        System.out.println(s1.equals(s2)+" "+ s1==s2);
+    }
+}
