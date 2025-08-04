3930f308877411f8be0e1fa46a81a9b7e97ec137
rvmalikrm <rm.rvmalik@gmail.com>
2025-08-04 14:25:03 +0530

    commit for Factorial using Resursion

---
+++ src/PracticeTest/Aug/factorialUsingRecursion.java
@@ -0,0 +1,15 @@
+package PracticeTest.Aug;
+
+public class factorialUsingRecursion {
+    public static void main(String[] args) {
+        int num =5;
+        int output =fact(num);
+        System.out.println(output);
+    }
+    public static int fact(int num){
+        if(num<=1){
+            return 1;
+        }
+        return  num*fact(num-1);
+    }
+}
