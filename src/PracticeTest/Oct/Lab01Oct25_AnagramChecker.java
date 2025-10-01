c145533066d5916a62267c7a64945f92878428d1
rvmalikrm <rm.rvmalik@gmail.com>
2025-10-01 11:41:40 +0530

    Lab01Oct25_AnagramChecker

---
+++ src/PracticeTest/Oct/Lab01Oct25_AnagramChecker.java
@@ -0,0 +1,42 @@
+package PracticeTest.Oct;
+
+public class Lab01Oct25_AnagramChecker {
+    public static void main(String[] args) {
+        String str1 = "listen";
+        String str2 = "silent";
+
+        if (areAnagrams(str1, str2)) {
+            System.out.println(str1 + " and " + str2 + " are anagrams.");
+        } else {
+            System.out.println(str1 + " and " + str2 + " are not anagrams.");
+        }
+    }
+
+    private static boolean areAnagrams(String str1, String str2) {
+        str1 = str1.replaceAll("\\s", "").toLowerCase();
+        str2 = str2.replaceAll("\\s", "").toLowerCase();
+
+        if (str1.length() != str2.length()) {
+            return false;
+        }
+
+        int[] charCount = new int[26]; // Assuming only lowercase letters a-z
+
+        for (char c : str1.toCharArray()) {
+            charCount[c - 'a']++;
+        }
+
+        for (char c : str2.toCharArray()) {
+            charCount[c - 'a']--;
+        }
+
+        for (int count : charCount) {
+            if (count != 0) {
+                return false;
+            }
+        }
+
+        return true;
+
+    }
+}
