b7ac9ef5014b3b092a940b1864dba1b0c1efa3f0
rvmalikrm <rm.rvmalik@gmail.com>
2025-10-09 15:32:09 +0530

    Lab09Oct25_Kadane_Alorith

---
+++ src/PracticeTest/Oct/Lab09Oct25_Kadane_Alorith.java
@@ -0,0 +1,13 @@
+package PracticeTest.Oct;
+public class Lab09Oct25_Kadane_Alorith {
+    public static void main(String[] args) {
+        int[] arr = {-2, 1, -3, 4, -1, 2, 1, -5, 4};
+        int maxEndingHere= arr[0];
+        int maxSoFar= arr[0];
+        for(int i=1;i<arr.length;i++){
+            maxEndingHere = Math.max(arr[i], maxEndingHere + arr[i]);
+            maxSoFar= Math.max(maxSoFar, maxEndingHere);
+        }
+        System.out.println(maxSoFar);
+    }
+}
