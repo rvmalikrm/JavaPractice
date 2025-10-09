00258ad6c498d6716c3ecb722d618cefb3e057fd
rvmalikrm <rm.rvmalik@gmail.com>
2025-10-09 13:14:13 +0530

    Lab09Oct25_FrequencyChecker

---
+++ src/PracticeTest/Oct/Lab09Oct25_FrequencyChecker.java
@@ -0,0 +1,18 @@
+package PracticeTest.Oct;
+import java.util.*;
+
+public class Lab09Oct25_FrequencyChecker {
+    public static void main(String[] args) {
+        String str= "aabbbcccddeeffghhijj";
+        Map<Character, Integer> map = new HashMap<>();
+        for(char ch:str.toCharArray()){
+            map.put(ch,map.getOrDefault(ch,0)+1);
+        }
+        System.out.println("Character Frequency:"+ map);
+        for(Map.Entry<Character,Integer> entry:map.entrySet()){
+            if(entry.getValue()>0){
+                System.out.println(entry.getKey()+" : "+entry.getValue());
+            }
+        }
+    }
+}
