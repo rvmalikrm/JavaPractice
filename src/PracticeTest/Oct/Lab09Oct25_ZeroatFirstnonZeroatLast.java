c3b892ed8664b89263408f3dea4f615c1e83d8f7
rvmalikrm <rm.rvmalik@gmail.com>
2025-10-09 15:01:31 +0530

    Lab09Oct25_ZeroatFirstnonZeroatLast

--- src/PracticeTest/Oct/Lab09Oct25_ZeroatFirstnonZeroatLast.java
+++ src/PracticeTest/Oct/Lab09Oct25_ZeroatFirstnonZeroatLast.java
@@ -4,28 +4,36 @@ import java.util.Arrays;
 
 public class Lab09Oct25_ZeroatFirstnonZeroatLast {
     public static void main(String[] args) {
-        int[] input=  {0,3,0,1,0,6,0,6,8,7,5,4,0,8,9,0,0,9,2,8,4};
-        int[] output= zeroatFirstnonZeroatLast(input);
-        System.out.println("Output array is:"+ Arrays.toString(output));
+        int[] input = {0, 3, 0, 1, 0, 6, 0, 6, 8, 7, 5, 4, 0, 8, 9, 0, 0, 9, 2, 8, 4};
+        int[] output = zeroAtFirstNonZeroAtLast(input);
+        System.out.println("Output array is: " + Arrays.toString(output));
     }
-    public static int[] zeroatFirstnonZeroatLast(int[] input){
-        int len= input.length;
-        int count=0;
-        int[] result= new int[len];
-        for(int n:input){
-            if(n==0){
-                result[n]=n;
-            }
-            else{
-                count++;
-            }
-        }
-        int i= len-count;
-        for(int n:input){
-            if(input[n]!=0){
-                result[i++]=input[n];
+
+    public static int[] zeroAtFirstNonZeroAtLast(int[] input) {
+        int len = input.length;
+        int[] result = new int[len];
+        int zeroCount = 0;
+
+        // First, count zeros
+        for (int num : input) {
+            if (num == 0) {
+                zeroCount++;
             }
         }
+
+        // Now, fill zeros at the beginning
+        for (int i = 0; i < zeroCount; i++) {
+            result[i] = 0;
+        }
+
+        // Then, fill non-zero elements after the zeros
+        int index = zeroCount;
+        for (int num : input) {
+            if (num != 0) {
+                result[index++] = num;
+            }
+        }
+
         return result;
     }
 }
