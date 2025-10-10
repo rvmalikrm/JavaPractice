c191045f6bde1d808a2b296f960e8f0f9ceb528b
rvmalikrm <rm.rvmalik@gmail.com>
2025-10-10 16:00:53 +0530

    Lab09Oct25_Kadane_Algo

---
+++ src/PracticeTest/Oct/Lab09Oct25_Kadane_Algo.java
@@ -0,0 +1,16 @@
+package PracticeTest.Oct;
+
+public class Lab09Oct25_Kadane_Algo {
+    public static void main(String[] args) {
+        int[] arr = {-2, 1, -3, 4, -1, 2, 1, -5, 4};
+        int maxSoFar = arr[0];
+        int maxEndingHere = arr[0];
+
+        for (int i = 1; i < arr.length; i++) {
+            maxEndingHere = Math.max(arr[i], maxEndingHere + arr[i]);
+            maxSoFar = Math.max(maxSoFar, maxEndingHere);
+        }
+
+        System.out.println("Maximum subarray sum is: " + maxSoFar);
+    }
+}
