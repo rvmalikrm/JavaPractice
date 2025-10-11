5277384fa92851cec475693b4c824608fa2bffbb
rvmalikrm <rm.rvmalik@gmail.com>
2025-10-11 23:57:59 +0530

    Lab11Oct25_PalindromeOrNot

---
+++ src/PracticeTest/Oct/Lab11Oct25_PalindromeOrNot.java
@@ -0,0 +1,18 @@
+package PracticeTest.Oct;
+
+import com.sun.tools.javac.Main;
+
+public class Lab11Oct25_PalindromeOrNot {
+    public static void main(String[] args) {
+        String str = "madam";
+        String rev = "";
+        for (int i = str.length() - 1; i >= 0; i--) {
+            rev += str.charAt(i);
+        }
+        if (str.equals(rev)) {
+            System.out.println(str + " is a palindrome");
+        } else {
+            System.out.println(str + " is not a palindrome");
+        }
+    }
+}
