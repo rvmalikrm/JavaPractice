562f3900740d5d02fe06a8f55bc8ce8dd495a5c7
rvmalikrm <rm.rvmalik@gmail.com>
2025-10-16 14:26:10 +0530

    Lab16Oct25_ReverseString

---
+++ src/PracticeTest/Oct/Lab16Oct25_RotateArray.java
@@ -0,0 +1,28 @@
+package PracticeTest.Oct;
+
+import java.util.Arrays;
+public class Lab16Oct25_RotateArray {
+    public static void main(String[] args) {
+        int[] arr={1,2,3,4,5};
+        int k=2; // number of rotations
+        rotateRight(arr,k);
+        System.out.println(Arrays.toString(arr));
+        }
+    public static void rotateRight(int[] arr,int k){
+        int n=arr.length;
+        reverse(arr,0,n-1);
+        reverse(arr,0,k-1);
+        reverse(arr,k,n-1);
+    }
+    public static void reverse(int[] arr,int start,int end){
+        while(start<end){
+            int temp=arr[start];
+            arr[start]=arr[end];
+            arr[end]=temp;
+            start++;
+            end--;
+        }
+    }
+
+}
+
