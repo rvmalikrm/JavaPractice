a44136a6037c014fc96079bcbcd887e77a71dd7c
rvmalikrm <rm.rvmalik@gmail.com>
2025-10-16 15:00:18 +0530

    Lab16Oct25_ReverseString

--- src/PracticeTest/Oct/Lab09Oct25_FrequencyChecker.java
+++ src/PracticeTest/Oct/Lab09Oct25_FrequencyChecker.java
@@ -3,16 +3,20 @@ import java.util.*;
 
 public class Lab09Oct25_FrequencyChecker {
     public static void main(String[] args) {
-        String str= "aabbbcccddeeffghhijj";
+        String str= "aabbbcccddeffghhijj";
         Map<Character, Integer> map = new HashMap<>();
         for(char ch:str.toCharArray()){
             map.put(ch,map.getOrDefault(ch,0)+1);
         }
         System.out.println("Character Frequency:"+ map);
         for(Map.Entry<Character,Integer> entry:map.entrySet()){
-            if(entry.getValue()>0){
+            int count=0;
+            if(entry.getValue()==1){
                 System.out.println(entry.getKey()+" : "+entry.getValue());
+                count++;
+
             }
+            System.out.println(count);
         }
     }
 }
