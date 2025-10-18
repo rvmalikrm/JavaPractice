3cf24ad70cba7e8af4ea85773ba22d02a09f14c2
rvmalikrm <rm.rvmalik@gmail.com>
2025-10-18 22:45:04 +0530

    Lab18Oct25_AnagramChecker

---
+++ src/PracticeTest/Oct/Lab18Oct25_AnagramChecker.java
@@ -0,0 +1,14 @@
+package PracticeTest.Oct;
+
+public class Lab18Oct25_AnagramChecker {
+     public static void main(String[] args) {
+        String str1 = "listen";
+        String str2 = "silent";
+
+        if (areAnagrams(str1, str2)) {
+            System.out.println(str1 + " and " + str2 + " are anagrams.");
+        } else {
+            System.out.println(str1 + " and " + str2 + " are not anagrams.");
+        }
+    }
+}
