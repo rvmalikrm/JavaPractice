a37aa1fde0be9549ccc102e629506acc58cac8b5
rvmalikrm <rm.rvmalik@gmail.com>
2025-10-27 17:01:51 +0530

    Lab24Oct25_reverseString

--- src/PracticeTest/Oct/Lab18Oct25_AnagramChecker.java
+++ src/PracticeTest/Oct/Lab18Oct25_AnagramChecker.java
@@ -1,18 +1,18 @@
-package PracticeTest.Oct;
-
-public class Lab18Oct25_AnagramChecker {
-     public static void main(String[] args) {
-        String str1 = "listen";
-        String str2 = "silent";
-
-        if (areAnagrams(str1, str2)) {
-            System.out.println(str1 + " and " + str2 + " are anagrams.");
-        } else {
-            System.out.println(str1 + " and " + str2 + " are not anagrams.");
-            System.out.println(str1 + " and " + str2 + " are not anagrams.");
-           // ystem.out.println(str1 + " and " + str2 + " are not anagrams.");
-        }
-    }
-}
-
-
+//package PracticeTest.Oct;
+//
+//public class Lab18Oct25_AnagramChecker {
+//     public static void main(String[] args) {
+//        String str1 = "listen";
+//        String str2 = "silent";
+//
+//        if (areAnagrams(str1, str2)) {
+//            System.out.println(str1 + " and " + str2 + " are anagrams.");
+//        } else {
+//            System.out.println(str1 + " and " + str2 + " are not anagrams.");
+//            System.out.println(str1 + " and " + str2 + " are not anagrams.");
+//           // ystem.out.println(str1 + " and " + str2 + " are not anagrams.");
+//        }
+//    }
+//}
+//
+//
