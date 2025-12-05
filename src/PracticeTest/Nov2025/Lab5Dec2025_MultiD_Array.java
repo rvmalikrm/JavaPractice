6263189e5b66d58aaf3501fb58fc1e11c25b9537
rvmalikrm <rm.rvmalik@gmail.com>
2025-12-05 11:51:50 +0530

    Lab5Dec2025_MultiDArrays

--- src/PracticeTest/Nov2025/Lab5Dec2025_MultiD_Array.java
+++ src/PracticeTest/Nov2025/Lab5Dec2025_MultiD_Array.java
@@ -1,4 +1,13 @@
 package PracticeTest.Nov2025;
 
 public class Lab5Dec2025_MultiD_Array {
+    public static void main(String[] args) {
+        int arr[][]= {{11,22,33},{44,55,66}};
+        for(int i=0;i<arr.length;i++){
+            for(int j=0;j<arr[i].length;j++){
+                System.out.print(arr[i][j]+" ");
+            }
+            System.out.println();
+        }
+    }
 }
