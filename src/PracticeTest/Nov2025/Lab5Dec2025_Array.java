6d8bfa5915c0f0f2c952ce1f2e7ff330fc641d5c
rvmalikrm <rm.rvmalik@gmail.com>
2025-12-05 11:32:51 +0530

    Lab5Dec2025_Array

---
+++ src/PracticeTest/Nov2025/Lab5Dec2025_Array.java
@@ -0,0 +1,13 @@
+package PracticeTest.Nov2025;
+
+public class Lab5Dec2025_Array {
+    public static void main(String[] args) {
+        // Array declaration and initialization and instantiation
+        int number[] = {10, 20, 30, 40, 50};
+        // Print arrya elements using for loop
+        //lenth whcih return the size of array
+        for (int i =0;i<number.length;i++){
+            System.out.println("number[" +i +"]: " +number[i]);
+        }
+    }
+}
