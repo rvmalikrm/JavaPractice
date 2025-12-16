4c3f359847d9e86e89b501d1928ed96e0d422088
rvmalikrm <rm.rvmalik@gmail.com>
2025-12-16 15:07:52 +0530

    Lab12Dec2025_RightRotateArray

---
+++ src/PracticeTest/Dec2025/Lab12Dec2025_RemoveDuplicateEleInArray.java
@@ -0,0 +1,14 @@
+package PracticeTest.Dec2025;
+
+public class Lab12Dec2025_RemoveDuplicateEleInArray {
+    public static void main(String[] args) {
+        // What is Array index out of bound Exception?
+        // occurs when we are access an array with an invalid index
+        // this happens when an array index is greater or equal to size of array
+        int number[] ={1,5,3,7,3,11};
+        for(int i=0;i<=number.length;i++){
+            System.out.println(number[i]);
+
+        }
+    }
+}
