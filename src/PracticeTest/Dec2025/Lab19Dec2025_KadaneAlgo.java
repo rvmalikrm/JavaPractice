a70324e0dc491aac10eb0b2475970ae8f708d075
rvmalikrm <rm.rvmalik@gmail.com>
2025-12-19 17:30:04 +0530

    Lab19Dec2025_KadaneAlgo

---
+++ src/PracticeTest/Dec2025/Lab19Dec2025_KadaneAlgo.java
@@ -0,0 +1,17 @@
+package PracticeTest.Dec2025;
+
+public class Lab19Dec2025_KadaneAlgo {
+    public static void main(String[] args) {
+        int[] arr = {-2, 1, -3, 4, -1, 2, 1, -5, 4};
+        int maxSoFar = arr[0];
+        int maxEndingHere = arr[0];
+
+        for (int i = 1; i < arr.length; i++) {
+            maxEndingHere = Math.max(arr[i], maxEndingHere + arr[i]);
+            maxSoFar = Math.max(maxSoFar, maxEndingHere);
+        }
+
+        System.out.println("Maximum subarray sum is: " + maxSoFar);
+
+    }
+}
